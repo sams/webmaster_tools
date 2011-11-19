ef142ec045ad906828d44477f676aace3a44861f
sam <sam.sherlock@gmail.com>
2011-11-19 23:50:27 +0000

    move exceptions to better place slim down componant pass message to exception

--- Controller/Component/MaintenanceComponent.php
+++ Controller/Component/MaintenanceComponent.php
@@ -15,37 +15,6 @@
  * @copyright  2010 David Persson <davidpersson@gmx.de>
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-
-/**
- * Parent class for all of the HTTP related exceptions in CakePHP.
- * All HTTP status/error related exceptions should extend this class so
- * catch blocks can be specifically typed.
- *
- * @package       Cake.Error
- */
-if (!class_exists('HttpException')) {
-	class HttpException extends RuntimeException { }
-}
-
-/**
- * Represents an HTTP 503 error.
- *
- * @package       WebmasterTool.Lib.Error
- */
-class MaintenanceException extends HttpException {
-/**
- * Constructor
- *
- * @param string $message If no message is given 'Bad Request' will be the message
- * @param string $code Status code, defaults to 400
- */
-	public function __construct($message = null, $code = 503) {
-		if (empty($message)) {
-			$message = 'Down for Maintenance';
-		}
-		parent::__construct($message, $code);
-	}
-}
 /**
  * Maintenance Component Class
  *
@@ -56,23 +25,20 @@ class MaintenanceComponent extends Component {
 	/**
 	 * Activates maintenance mode.
 	 *
-	 * Disables debug mode (if activated for i.e. admin) and sets
-	 * an appropriate header.
+	 * Disables debug mode (if activated for i.e. admin) throwing an exception.
 	 *
 	 * Example usage:
 	 * {{{
-	 * if (!$isAdmin && Configure::read('Server.maintenance')) {
+	 * if (!$isAdmin && Configure::read('WebmasterTools.Maintenance.active')) {
 	 *	$this->Maintenance->activate();
 	 * }
 	 * }}}
 	 *
 	 * @return void
-	 * @link http://mark-story.com/posts/view/quick-and-dirty-down-for-maintenance-page-with-cakephp
 	 */
 	public function activate($message = null) {
-		Configure::write('debug', 2);
-
-		throw new MaintenanceException('Down for some Mainentance');
+		Configure::write('debug', 0);
+		throw new MaintenanceException($message);
 	}
 }
 
