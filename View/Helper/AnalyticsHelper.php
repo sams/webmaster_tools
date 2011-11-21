04a677452794f62416ac54d4da5f6fe77b0f5d45
sam <sam.sherlock@gmail.com>
2011-11-21 11:45:28 +0000

    add app::uses to helpers

--- View/Helper/AnalyticsHelper.php
+++ View/Helper/AnalyticsHelper.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * Analytics Helper File
+ *
+ * Copyright (c) 2010 David Persson
+ *
+ * Distributed under the terms of the MIT License.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * PHP version 5
+ * CakePHP version 1.3
+ *
+ * @package    webmaster_tools
+ * @subpackage webmaster_tools.views.helpers
+ * @copyright  2010 David Persson <davidpersson@gmx.de>
+ * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+App::uses('AppHelper', 'View/Helper');
 class AnalyticsHelper extends AppHelper {
 
 	const CUSTOM_VAR_MAX_LENGTH = 64;
