cbabf6ebc837d3f4bdd3b7616a9bbc86208d7d24
sam <sam.sherlock@gmail.com>
2011-11-21 12:01:39 +0000

    update RobotControl Helper Test

--- Test/Case/View/Helper/RobotControlHelperTest.php
+++ Test/Case/View/Helper/RobotControlHelperTest.php
@@ -40,8 +40,6 @@ class TestController extends Controller {
  */
 class RobotControlHelperTest extends CakeTestCase {
 
-	public $Helper;
-
 	protected $_online;
 
 	public function setUp() {
@@ -58,34 +56,149 @@ class RobotControlHelperTest extends CakeTestCase {
 
 	public function testAllow() {
 
+		$this->RobotControl->allow('/css/');
+		$this->RobotControl->allow('/img/');
+
+		$result = $this->RobotControl->generate();
+		$expected = <<<TXT
+User-agent: *
+Allow: /css/
+Allow: /img/
+
+TXT;
+		$this->assertEqual($expected, $result);
 	}
 
 	public function testDeny() {
+		$this->RobotControl->deny('/secret/');
+
+		$result = $this->RobotControl->generate();
+		$expected = <<<TXT
+User-agent: *
+Disallow: /secret/
+
+TXT;
+		$this->assertEqual($expected, $result);
 
 	}
 
 	public function testSitemap() {
 
+		$this->RobotControl->sitemap('/sitemap.xml');
+
+		$result = $this->RobotControl->generate();
+		$expectedUrl = FULL_BASE_URL . '/sitemap.xml';
+		$expected = <<<TXT
+User-agent: *
+Sitemap: {$expectedUrl}
+
+TXT;
+		$this->assertEqual($expected, $result);
+
+	}
+
+	public function testCrawlBlocks() {
+		$this->RobotControl->allow('/test0/');
+		$this->RobotControl->allow('/test1/', 'agent0');
+		$this->RobotControl->allow('/test2/');
+
+		$result = $this->RobotControl->generate(array('reset' => true));
+		$expected = <<<TXT
+User-agent: agent0
+Allow: /test1/
+
+User-agent: *
+Allow: /test0/
+Allow: /test2/
+
+TXT;
+		$this->assertEqual($expected, $result);
+
+		$this->RobotControl->allow('/test0/', 'agent0');
+		$this->RobotControl->allow('/test1/', 'agent1');
+		$this->RobotControl->allow('/test2/', 'agent2');
+		$this->RobotControl->allow('/test3/');
+		$this->RobotControl->allow('/test4/', 'agent2');
+
+		$result = $this->RobotControl->generate(array('reset' => true));
+		$expected = <<<TXT
+User-agent: agent2
+Allow: /test2/
+Allow: /test4/
+
+User-agent: agent1
+Allow: /test1/
+
+User-agent: agent0
+Allow: /test0/
+
+User-agent: *
+Allow: /test3/
+
+TXT;
+		$this->assertEqual($expected, $result);
 	}
 
 	public function testCrawlDelay() {
+		$this->RobotControl->crawlDelay(30);
 
+		$result = $this->RobotControl->generate();
+		$expected = <<<TXT
+User-agent: *
+Crawl-delay: 30
+
+TXT;
+		$this->assertEqual($expected, $result);
 	}
 
 	public function testVisitTime() {
 
+		$this->RobotControl->visitTime('13:00', '20:00');
+
+		$result = $this->RobotControl->generate();
+		$expected = <<<TXT
+User-agent: *
+Visit-time: 13:00 - 20:00
+
+TXT;
+		$this->assertEqual($expected, $result);
 	}
 
 	public function testRequestRate() {
+		$this->RobotControl->requestRate(20, '13:00', '20:00', 'agent0');
+		$this->RobotControl->requestRate(20, '13:00', null, 'agent1');
+		$this->RobotControl->requestRate(20, null, null, 'agent2');
 
+		$result = $this->RobotControl->generate();
+		$expected = <<<TXT
+User-agent: agent2
+Request-rate: 20/60
+
+User-agent: agent1
+Request-rate: 20/60
+
+User-agent: agent0
+Request-rate: 20/60 13:00 - 20:00
+
+TXT;
+		$this->assertEqual($expected, $result);
 	}
 
 	public function testComment() {
 
 	}
 
-	public function testGenerate() {
+	public function testReset() {
+		$this->RobotControl->allow('/css/');
+		$resultA = $this->RobotControl->generate();
+		$resultB = $this->RobotControl->generate();
 
+		$this->assertEqual($resultA, $resultB);
+
+		$resultA = $this->RobotControl->generate(array('reset' => true));
+		$resultB = $this->RobotControl->generate();
+
+		$this->assertNotEqual($resultA, $resultB);
 	}
 
 }
