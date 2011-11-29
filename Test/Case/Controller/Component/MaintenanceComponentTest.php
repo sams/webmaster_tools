61b4ba9ee7f3f986a3141defe63405f976adfc77
sam <sam.sherlock@gmail.com>
2011-11-29 22:59:55 +0000

    update tests inline with recent updates

--- Test/Case/Controller/Component/MaintenanceComponentTest.php
+++ Test/Case/Controller/Component/MaintenanceComponentTest.php
@@ -37,13 +37,42 @@ class ArticlesTestController extends Controller {
  * 
  */
 	public function beforeFilter() {
-		$this->Maintenance->actiavte('close for now');
+		$this->Maintenance->activate();
 	}
 
 /**
  * 
  */
 	public function closed_for_maintenance() {
+	}
+
+}
+
+class Articles2TestController extends Controller {
+
+/**
+ * @var string
+ * @access public
+ */
+	public $name = 'ArticlesTest';
+
+/**
+ * @var array
+ * @access public
+ */
+	public $uses = array('Article');
+
+/**
+ * @var array
+ * @access public
+ */
+	public $components = array('WebmasterTools.Maintenance');
+
+/**
+ * 
+ */
+	public function index() {
+		$this->Maintenance->activate('closed for now');
 	}
 
 }
@@ -69,8 +98,8 @@ class MaintenanceComponentTestCase extends CakeTestCase {
 			'url' => array());
 		$this->Controller->modelClass = 'Article';
 
-		$this->Maintenance = new MaintenanceComponent($this->Controller);
-		$this->Controller->_Config = $config;
+		$this->Maintenance = new MaintenanceComponent($this->Controller->Components);
+		//$this->_config = $config;
 		$this->_debug = Configure::read('debug');
 	}
 
@@ -92,10 +121,43 @@ class MaintenanceComponentTestCase extends CakeTestCase {
  * @return void
  */
 	public function testActivate() {
-		$this->Controller->beforeFilter();
-		$this->Controller->closed_for_maintenance();
-		//$this->Maintenance->actiavte();
+			Configure::write('WebmasterTools.Maintenance', array(
+				'active' => false,
+				'message' => 'Not much to see here',
+				'title' => 'We are in Maintenance Mode',
+				'revisit' => MONTH,
+				'layout' => 'ajax'
+			));
+		try {
+			$this->Controller->beforeFilter();
+			$this->Controller->closed_for_maintenance();
+		} catch (Exception $e) {
+			Configure::write('debug', 2);
+			$this->assertInstanceOf('MaintenanceException', $e);
+			Configure::write('debug', 0);
+		}
 
+	}
+	
+	public function testSuspend() {
+		$this->Controller = new Articles2TestController();
+		$this->Controller->constructClasses();
+		$this->Controller->action = 'index';
+		$this->Controller->params = array(
+			'named' => array(),
+			'pass' => array(),
+			'url' => array());
+		$this->Controller->modelClass = 'Article';
+
+		$this->Maintenance = new MaintenanceComponent($this->Controller->Components);
+		try {
+			$this->Controller->beforeFilter();
+			$this->Controller->index();
+		} catch (Exception $e) {
+			Configure::write('debug', 2);
+			$this->assertInstanceOf('MaintenanceException', $e);
+			Configure::write('debug', 0);
+		}
 	}
 
 }
